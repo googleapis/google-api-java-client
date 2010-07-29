b23892e6641579ae7300753f22bd833fac5ea689
Yaniv Inbar <yanivi@google.com>
2010-07-29 15:55:13 -0400

    Separate pom.xml files for each Java package.

--- generator/com/google/api/client/generator/GeneratePom.java
+++ generator/com/google/api/client/generator/GeneratePom.java
@@ -20,6 +20,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * @author Yaniv Inbar
@@ -28,15 +30,46 @@ public class GeneratePom {
 
   public static void main(String[] args) throws IOException {
     File googleApiClientDirectory = Generation.getDirectory(args[0]);
+    // package names
+    SortedSet<String> packageNames =
+        computePackageNames(googleApiClientDirectory);
     // compute file generators
     List<AbstractFileGenerator> fileGenerators =
         new ArrayList<AbstractFileGenerator>();
-    fileGenerators.add(
-        new PomFileGenerator(new File(googleApiClientDirectory, "src")));
-    Generation.compute(
-        fileGenerators, new File(googleApiClientDirectory, "target"));
+    fileGenerators.add(new PomFileGenerator(packageNames));
+    for (String packageName : packageNames) {
+      fileGenerators.add(new ModulePomFileGenerator(packageName));
+    }
+    Generation.compute(fileGenerators, googleApiClientDirectory);
   }
 
   private GeneratePom() {
+  }
+
+  private static SortedSet<String> computePackageNames(
+      File googleApiClientDirectory) throws IOException {
+    SortedSet<String> packageNames = new TreeSet<String>();
+    File srcDirectory = new File(googleApiClientDirectory, "src/com");
+    int rootPathLength = srcDirectory.getCanonicalPath().length();
+    addPackageNames(rootPathLength, srcDirectory, packageNames);
+    return packageNames;
+  }
+
+  private static void addPackageNames(
+      int rootPathLength, File dir, SortedSet<String> packageNames)
+      throws IOException {
+    for (File file : dir.listFiles()) {
+      if (".svn".equals(file.getName())) {
+        continue;
+      }
+      if (file.isDirectory()) {
+        addPackageNames(rootPathLength, file, packageNames);
+      } else {
+        if (file.getName().endsWith(".java")) {
+          packageNames.add(file.getParentFile().getCanonicalPath().substring(
+              1 + rootPathLength).replace('/', '-'));
+        }
+      }
+    }
   }
 }
