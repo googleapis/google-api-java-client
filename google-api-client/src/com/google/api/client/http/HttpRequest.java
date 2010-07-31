9a69bcf9cba4d9f263b5051633f0f243959089cf
Yaniv Inbar <yanivi@google.com>
2010-07-31 15:04:57 -0400

    Prepare 1.0.10-alpha

--- google-api-client/src/com/google/api/client/http/HttpRequest.java
+++ google-api-client/src/com/google/api/client/http/HttpRequest.java
@@ -35,7 +35,7 @@ public final class HttpRequest {
 
   /** User agent suffix for all requests. */
   private static final String USER_AGENT_SUFFIX =
-      "Google-API-Java-Client/1.0.9-alpha";
+      "Google-API-Java-Client/1.0.10-alpha";
 
   /**
    * HTTP request headers.
