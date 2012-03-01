1f21de1fcb5563f761c32142b9021f5c8bf6b738
Yaniv Inbar <yanivi@google.com>
2012-02-29 19:17:47 -0500

    Deprecate Google OAuth 1.0
    http://codereview.appspot.com/5715043/

--- google-api-client/src/main/java/com/google/api/client/googleapis/auth/oauth/package-info.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/auth/oauth/package-info.java
@@ -107,6 +107,8 @@ keytool -export -rfc -keystore ./Example.jks -storepass changeme \
  *
  * @since 1.0
  * @author Yaniv Inbar
+ * @deprecated (scheduled to be removed in 1.8) Use
+ *             {@link com.google.api.client.googleapis.auth.oauth2}
  */
 
 package com.google.api.client.googleapis.auth.oauth;
