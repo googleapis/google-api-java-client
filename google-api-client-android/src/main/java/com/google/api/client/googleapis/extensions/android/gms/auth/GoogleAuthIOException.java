46f463973771ddb2fe03fe0371c6552f253d13d7
Yaniv Inbar <yanivi@google.com>
2012-10-16 12:09:52 -0400

    [api] GoogleAuthIOException and UserRecoverableAuthIOException
    https://codereview.appspot.com/6709044/

---
+++ google-api-client-android/src/main/java/com/google/api/client/googleapis/extensions/android/gms/auth/GoogleAuthIOException.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.googleapis.extensions.android.gms.auth;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.common.base.Preconditions;
+
+import java.io.IOException;
+
+/**
+ * Wraps a {@link GoogleAuthException} into an {@link IOException} so it can be caught directly.
+ *
+ * <p>
+ * Use {@link #getCause()} to get the wrapped {@link GoogleAuthException}.
+ * </p>
+ *
+ * @since 1.12
+ * @author Yaniv Inbar
+ */
+public class GoogleAuthIOException extends IOException {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @param wrapped wrapped {@link GoogleAuthException}
+   */
+  GoogleAuthIOException(GoogleAuthException wrapped) {
+    initCause(Preconditions.checkNotNull(wrapped));
+  }
+
+  @Override
+  public GoogleAuthException getCause() {
+    return (GoogleAuthException) super.getCause();
+  }
+}
