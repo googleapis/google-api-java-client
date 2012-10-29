5fe61063da0493d6602c05fc0267f957b5229b72
Ravi Mistry <rmistry@google.com>
2012-10-29 13:36:47 -0400

    Adding VERSION to GoogleUtils.java https://codereview.appspot.com/6810049/

---
+++ google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
@@ -0,0 +1,26 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.googleapis;
+
+/**
+ * Utility class for the Google API Client Library.
+ *
+ * @since 1.12
+ * @author rmistry@google.com (Ravi Mistry)
+ */
+public class GoogleUtils {
+
+  /** Current version of the Google API Client Library for Java. */
+  public static final String VERSION = "1.13.0-beta-SNAPSHOT";
+
+}
