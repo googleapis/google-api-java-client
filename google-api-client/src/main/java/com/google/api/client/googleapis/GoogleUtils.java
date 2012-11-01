fcd6842eeed6b9052a42332939e8fd4da6ff254d
Ravi Mistry <rmistry@google.com>
2012-11-01 08:00:34 -0400

    start 1.12.1-beta

--- google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
@@ -21,6 +21,6 @@ package com.google.api.client.googleapis;
 public class GoogleUtils {
 
   /** Current version of the Google API Client Library for Java. */
-  public static final String VERSION = "1.12.0-beta";
+  public static final String VERSION = "1.12.1-beta-SNAPSHOT";
 
 }
