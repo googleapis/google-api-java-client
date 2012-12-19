cdfb4e30af72bbdffd0c7f0ee3bf7db8d1f7448e
Nick Miceli <ngmiceli@google.com>
2012-12-19 15:45:35 -0500

    prepare 1.13.1-beta for release

--- google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
@@ -21,6 +21,6 @@ package com.google.api.client.googleapis;
 public class GoogleUtils {
 
   /** Current version of the Google API Client Library for Java. */
-  public static final String VERSION = "1.13.1-beta-SNAPSHOT";
+  public static final String VERSION = "1.13.1-beta";
 
 }
