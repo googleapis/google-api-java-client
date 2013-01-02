381702d89a2f71ecdcd9b6afa02afb23adcc4485
Nick Miceli <ngmiceli@google.com>
2013-01-02 16:27:26 -0500

    Start 1.13.3

--- google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
@@ -21,6 +21,6 @@ package com.google.api.client.googleapis;
 public class GoogleUtils {
 
   /** Current version of the Google API Client Library for Java. */
-  public static final String VERSION = "1.13.2-beta";
+  public static final String VERSION = "1.13.3-SNAPSHOT";
 
 }
