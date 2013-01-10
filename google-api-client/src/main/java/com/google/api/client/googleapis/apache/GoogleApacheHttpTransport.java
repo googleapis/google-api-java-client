ac0440932157c1faa5d2dd147a972bfcac12fc98
Yaniv Inbar <yanivi@google.com>
2013-01-10 10:46:33 -0500

    GoogleNetHttpTransport & GoogleApacheHttpTransport for easier control over package deps
    add GoogleUtils.MAJOR_VERSION, MINOR_VERSION, and BUGFIX_VERSION ints to make it easier to build logic around the version parts.
    
    https://codereview.appspot.com/7073057/

---
+++ google-api-client/src/main/java/com/google/api/client/googleapis/apache/GoogleApacheHttpTransport.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.googleapis.apache;
+
+import com.google.api.client.googleapis.GoogleUtils;
+import com.google.api.client.http.apache.ApacheHttpTransport;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+/**
+ * Utilities for Google APIs based on {@link ApacheHttpTransport}.
+ *
+ * @since 1.14
+ * @author Yaniv Inbar
+ */
+public final class GoogleApacheHttpTransport {
+
+  /**
+   * Returns a new instance of {@link ApacheHttpTransport} that uses
+   * {@link #getCertificateTrustStore()} for the trusted certificates using
+   * {@link ApacheHttpTransport.Builder#trustCertificates(KeyStore)}.
+   */
+  public static ApacheHttpTransport newTrustedTransport() throws GeneralSecurityException,
+      IOException {
+    return new ApacheHttpTransport.Builder().trustCertificates(
+        GoogleUtils.getCertificateTrustStore()).build();
+  }
+
+  private GoogleApacheHttpTransport() {
+  }
+}
