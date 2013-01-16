b0f0be64f76869b24d9ca3853f3c76a7ee324bb6
Yaniv Inbar <yanivi@google.com>
2013-01-16 12:02:23 -0500

    better JavaDoc on GoogleNetHttpTransport.newTrustedTransport()
    https://codereview.appspot.com/7140043/

--- google-api-client/src/main/java/com/google/api/client/googleapis/javanet/GoogleNetHttpTransport.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/javanet/GoogleNetHttpTransport.java
@@ -33,6 +33,21 @@ public class GoogleNetHttpTransport {
    * Returns a new instance of {@link NetHttpTransport} that uses
    * {@link GoogleUtils#getCertificateTrustStore()} for the trusted certificates using {@link com.google.api.client.http.javanet.NetHttpTransport.Builder#trustCertificates(KeyStore)}
    * .
+   *
+   * <p>
+   * This helper method doesn't provide for customization of the {@link NetHttpTransport}, such as
+   * the ability to specify a proxy. To do use, use
+   * {@link com.google.api.client.http.javanet.NetHttpTransport.Builder}, for example:
+   * </p>
+   *
+   * <pre>
+  static HttpTransport newProxyTransport() throws GeneralSecurityException, IOException {
+    NetHttpTransport.Builder builder = new NetHttpTransport.Builder();
+    builder.trustCertificates(GoogleUtils.getCertificateTrustStore());
+    builder.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 3128)));
+    return builder.build();
+  }
+   * </pre>
    */
   public static NetHttpTransport newTrustedTransport()
       throws GeneralSecurityException, IOException {
