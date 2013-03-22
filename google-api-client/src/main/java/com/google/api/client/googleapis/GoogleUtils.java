e68edcdda4c13ee2e6e638b11f618166b1e6339a
Yaniv Inbar <yanivi@google.com>
2013-03-22 13:32:55 -0400

    api: suppress compiler inlining of GoogleUtils.java
    https://codereview.appspot.com/7784045/

--- google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
@@ -51,8 +51,9 @@ public final class GoogleUtils {
   public static final int BUGFIX_VERSION = 1;
 
   /** Current release version. */
-  public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + BUGFIX_VERSION
-      + "-beta";
+  // NOTE: toString() so compiler thinks it isn't a constant, so it won't inline it
+  public static final String VERSION = (MAJOR_VERSION + "." + MINOR_VERSION + "." + BUGFIX_VERSION
+      + "-beta").toString();
 
   /** Cached value for {@link #getCertificateTrustStore()}. */
   static KeyStore certTrustStore;
