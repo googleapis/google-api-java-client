8ea759e12c9a2c0c39df202d0979ae4d1f653421
Eyal Peled <peleyal@google.com>
2013-04-03 01:18:47 -0400

    api issue 755: Specify public certs URL for ID Token verification https://codereview.appspot.com/8069045/

--- google-api-client/src/main/java/com/google/api/client/googleapis/auth/oauth2/GoogleOAuthConstants.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/auth/oauth2/GoogleOAuthConstants.java
@@ -14,6 +14,8 @@
 
 package com.google.api.client.googleapis.auth.oauth2;
 
+import com.google.api.client.util.Beta;
+
 /**
  * Constants for Google's OAuth 2.0 implementation.
  *
@@ -27,6 +29,16 @@ public class GoogleOAuthConstants {
 
   /** Encoded URL of Google's token server. */
   public static final String TOKEN_SERVER_URL = "https://accounts.google.com/o/oauth2/token";
+
+  /**
+   * {@link Beta} <br/>
+   * Encoded URL of Google's public certificates.
+   *
+   * @since 1.15
+   */
+  @Beta
+  public static final String DEFAULT_PUBLIC_CERTS_ENCODED_URL =
+      "https://www.googleapis.com/oauth2/v1/certs";
 
   /**
    * Redirect URI to use for an installed application as specified in <a
