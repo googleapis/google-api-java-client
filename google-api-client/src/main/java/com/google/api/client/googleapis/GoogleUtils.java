e1dd19c3b7e95d7cf7f08b3e386ffdd02374c564
Yaniv Inbar <yanivi@google.com>
2013-05-18 13:38:55 -0400

    Backed out changeset fac965078693

--- google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/GoogleUtils.java
@@ -42,7 +42,7 @@ public final class GoogleUtils {
    *
    * @since 1.14
    */
-  public static final Integer MINOR_VERSION = 17;
+  public static final Integer MINOR_VERSION = 16;
 
   /**
    * Bug fix part of the current release version.
