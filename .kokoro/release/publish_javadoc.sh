d90653b5085675bf0510e36456064f5c2b284933
yoshi-automation <yoshi-automation@google.com>
2019-11-14 00:39:40 -0800

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- .kokoro/release/publish_javadoc.sh
+++ .kokoro/release/publish_javadoc.sh
@@ -33,7 +33,7 @@ python3 -m pip install gcp-docuploader
 # compile all packages
 mvn clean install -B -DskipTests=true
 
-NAME=google-api-client
+NAME=google-api-java-client
 VERSION=$(grep ${NAME}: versions.txt | cut -d: -f3)
 
 # build the docs
