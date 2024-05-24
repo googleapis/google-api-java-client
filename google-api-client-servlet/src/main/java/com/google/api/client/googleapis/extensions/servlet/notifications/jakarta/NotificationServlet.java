e928cfa159b9bce81214e0fb94e49ed034f23056
Tomo Suzuki <suztomo@gmail.com>
2024-05-24 11:28:49 -0400

    feat: WebHookUtil classes using the jakarta namespace (#2484)
    * feat: WebHookUtil classes using the jakarta namespace
    
    Fixes #2260
    
    Copied the implementation with the jakarta.servlet namespace
    using the same methology as https://github.com/googleapis/google-oauth-java-client/pull/1115.
    
    - Replaced javax with jakarta and declared the dependency with
      provided scope.
    - Keeping the classes as `Beta` annotated.
    - No tests exist for the classes.
    - Added `@since 2.6.0` as the last release was 2.5.1.
    
    * 🦉 Updates from OwlBot post-processor
    
    See https://github.com/googleapis/repo-automation-bots/blob/main/packages/owl-bot/README.md
    
    ---------
    
    Co-authored-by: Owl Bot <gcf-owl-bot[bot]@users.noreply.github.com>

---
+++ google-api-client-servlet/src/main/java/com/google/api/client/googleapis/extensions/servlet/notifications/jakarta/NotificationServlet.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.client.googleapis.extensions.servlet.notifications.jakarta;
+
+import com.google.api.client.googleapis.notifications.StoredChannel;
+import com.google.api.client.util.Beta;
+import com.google.api.client.util.store.DataStore;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.MemoryDataStoreFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * {@link Beta} <br>
+ * Thread-safe Webhook Servlet to receive notifications using the {@code jakarta.servlet} namespace.
+ *
+ * <p>In order to use this servlet you should create a class inheriting from {@link
+ * NotificationServlet} and register the servlet in your web.xml.
+ *
+ * <p>It is a simple wrapper around {@link WebhookUtils#processWebhookNotification}, so if you you
+ * may alternatively call that method instead from your {@link HttpServlet#doPost} with no loss of
+ * functionality. <b>Example usage:</b>
+ *
+ * <pre>{@code
+ * public class MyNotificationServlet extends NotificationServlet {
+ *
+ *   private static final long serialVersionUID = 1L;
+ *
+ *   public MyNotificationServlet() throws IOException {
+ *     super(new SomeDataStoreFactory());
+ *   }
+ * }
+ * }</pre>
+ *
+ * <b>Sample web.xml setup:</b>
+ *
+ * <pre>{@code
+ * {@literal <}servlet{@literal >}
+ * {@literal <}servlet-name{@literal >}MyNotificationServlet{@literal <}/servlet-name{@literal >}
+ * {@literal <}servlet-class{@literal >}
+ *     com.mypackage.MyNotificationServlet
+ * {@literal <}/servlet-class{@literal >}
+ * {@literal <}/servlet{@literal >}
+ * {@literal <}servlet-mapping{@literal >}
+ * {@literal <}servlet-name{@literal >}MyNotificationServlet{@literal <}/servlet-name{@literal >}
+ * {@literal <}url-pattern{@literal >}/notifications{@literal <}/url-pattern{@literal >}
+ * {@literal <}/servlet-mapping{@literal >}
+ * }</pre>
+ *
+ * <p>WARNING: by default it uses {@link MemoryDataStoreFactory#getDefaultInstance()} which means it
+ * will NOT persist the notification channels when the servlet process dies, so it is a BAD CHOICE
+ * for a production application. But it is a convenient choice when testing locally, in which case
+ * you don't need to override it, and can simply reference it directly in your web.xml file. For
+ * example:
+ *
+ * <pre>{@code
+ * {@literal <}servlet{@literal >}
+ * {@literal <}servlet-name{@literal >}NotificationServlet{@literal <}/servlet-name{@literal >}
+ * {@literal <}servlet-class{@literal >}
+ *     com.google.api.client.googleapis.extensions.servlet.notificationsNotificationServlet
+ * {@literal <}/servlet-class{@literal >}
+ * {@literal <}/servlet{@literal >}
+ * {@literal <}servlet-mapping{@literal >}
+ * {@literal <}servlet-name{@literal >}NotificationServlet{@literal <}/servlet-name{@literal >}
+ * {@literal <}url-pattern{@literal >}/notifications{@literal <}/url-pattern{@literal >}
+ * {@literal <}/servlet-mapping{@literal >}
+ * }</pre>
+ *
+ * @since 2.6.0
+ */
+@Beta
+public class NotificationServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  /** Notification channel data store. */
+  private final transient DataStore<StoredChannel> channelDataStore;
+
+  /**
+   * Constructor to be used for testing and demo purposes that uses {@link
+   * MemoryDataStoreFactory#getDefaultInstance()} which means it will NOT persist the notification
+   * channels when the servlet process dies, so it is a bad choice for a production application.
+   */
+  public NotificationServlet() throws IOException {
+    this(MemoryDataStoreFactory.getDefaultInstance());
+  }
+
+  /**
+   * Constructor which uses {@link StoredChannel#getDefaultDataStore(DataStoreFactory)} on the given
+   * data store factory, which is the normal use case.
+   *
+   * @param dataStoreFactory data store factory
+   */
+  protected NotificationServlet(DataStoreFactory dataStoreFactory) throws IOException {
+    this(StoredChannel.getDefaultDataStore(dataStoreFactory));
+  }
+
+  /**
+   * Constructor that allows a specific notification data store to be specified.
+   *
+   * @param channelDataStore notification channel data store
+   */
+  protected NotificationServlet(DataStore<StoredChannel> channelDataStore) {
+    this.channelDataStore = channelDataStore;
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    WebhookUtils.processWebhookNotification(req, resp, channelDataStore);
+  }
+}
