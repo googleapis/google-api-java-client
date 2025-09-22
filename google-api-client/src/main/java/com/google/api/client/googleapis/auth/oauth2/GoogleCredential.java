ade0ee08a344d501630a4251e852d59ba6c7e490
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2025-09-22 16:23:20 -0400

    docs: match deprecated message with guidance

--- google-api-client/src/main/java/com/google/api/client/googleapis/auth/oauth2/GoogleCredential.java
+++ google-api-client/src/main/java/com/google/api/client/googleapis/auth/oauth2/GoogleCredential.java
@@ -222,9 +222,7 @@ public class GoogleCredential extends Credential {
    * @param credentialStream the stream with the credential definition.
    * @return the credential defined by the credentialStream.
    * @throws IOException if the credential cannot be created from the stream.
-   * @deprecated Please use <a href="https://github.com/googleapis/google-auth-library-java">
-   *     google-auth-library</a> for handling Application Default Credentials and other non-OAuth2
-   *     based authentication.
+   * @deprecated This method is being deprecated because of a potential security risk.
    */
   @Beta
   @Deprecated
@@ -259,9 +257,7 @@ public class GoogleCredential extends Credential {
    * @param credentialStream the stream with the credential definition.
    * @return the credential defined by the credentialStream.
    * @throws IOException if the credential cannot be created from the stream.
-   * @deprecated Please use <a href="https://github.com/googleapis/google-auth-library-java">
-   *     google-auth-library</a> for handling Application Default Credentials and other non-OAuth2
-   *     based authentication.
+   * @deprecated This method is being deprecated because of a potential security risk.
    */
   @Beta
   @Deprecated
